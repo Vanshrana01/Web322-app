871d5089a28412aa70fdce6454ea78de5aeea31d
Vansh Rana <vranan15@myseneca.ca>
2023-06-02 13:01:12 -0400

    third commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect('/about');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Express http server listening on port ${port}`);
+});
+
+
+
+
+
