df5f231b876974ddbb3d404e85bdf5b5f1d970c1
Vansh Rana <vranan15@myseneca.ca>
2023-06-02 20:53:14 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,20 +1,78 @@
-const express = require('express');
-const app = express();
+/*********************************************************************************
 
-app.use(express.static('public'));
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Vansh Rana
+Student ID: 169672219 
+Date: 2 June 2023
+Cyclic Web App URL: 
+GitHub Repository URL: 
+
+********************************************************************************/ 
+
+const express = require('express')
+const store_service = require('./store-service')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.static('public')); 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.redirect('/about');
+  res.redirect("/about")
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
+    res.sendFile(__dirname + "/views/about.html")
+  });
+
+app.get('/shop', (req, res) => {
+  store_service.getPublishedItems().then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    return {'message': err}
+  })
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Express http server listening on port ${port}`);
+app.get('/items', (req, res) => {
+  store_service.getAllItems().then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    return {'message': err}
+  })
 });
+
+app.get('/categories', (req, res) => {
+    store_service.getCategories().then((data)=>{
+      res.json(data)
+    }).catch((err)=>{
+      return {'message': err}
+    })
+  });
+
+
+  app.get('*', function(req, res){
+    res.send('Page not found, check URL', 404);
+  });
+
+
+
+function onHTTPstart(){
+  console.log("server started on port: " + port)
+}
+
+store_service.initialize().then(function(){
+  app.listen(port,onHTTPstart);
+}).catch(function(err){
+  console.log("unable to start" + err)
+})
+
+
+app.use((req,res)=>{
+  res.status(404).send("Page does not exist")
+})
+
 
 
 
