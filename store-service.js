c68d88d7a27ffece2842a8e9eb45c01b3d2f1495
Vansh Rana <vranan15@myseneca.ca>
2023-07-16 04:00:16 -0400

    initial commit

--- store-service.js
+++ store-service.js
@@ -23,60 +23,101 @@ const Category = sequelize.define('category', {
     category: Sequelize.STRING
 });
 
-Item.belongsTo(Category, {foreignKey: 'category'});
+Item.belongsTo(Category, { foreignKey: 'category' });
 
 module.exports.initialize = () => {
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.sync()
+            .then(() => {
+                Category.sync()
+                    .then(() => resolve())
+                    .catch(() => reject('Unable to sync the database'));
+            })
+            .catch(() => reject('Unable to sync the database'));
+    });
 };
 
 
 module.exports.getAllItems = () => {
     return new Promise((resolve, reject) => {
-        reject();
-    })
-};
+        Item.findAll()
+            .then((items) => resolve(items))
+            .catch(() => reject('No results returned'));
+    });
+}
+
 
 module.exports.getPublishedItems = () => {
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.findAll({ where: { published: true } })
+            .then((items) => resolve(items))
+            .catch(() => reject('No results returned'));
+    });
 };
 
 module.exports.getCategories = () => {
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Category.findAll()
+            .then((categories) => resolve(categories))
+            .catch(() => reject('No results returned'));
+    });
 };
 
+
 module.exports.addItem = (itemData) => {
+    itemData.published = itemData.published ? true : false;
+
+    for (const prop in itemData) {
+        if (itemData[prop] === '') {
+            itemData[prop] = null;
+        }
+    }
+
+    itemData.postDate = new Date();
+
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.create(itemData)
+            .then(() => resolve())
+            .catch(() => reject('Unable to create post'));
+    });
 };
 
 module.exports.getItemsByCategory = (category) => {
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.findAll({ where: { category } })
+            .then((items) => resolve(items))
+            .catch(() => reject('No results returned'));
+    });
 };
 
-
 module.exports.getItemsByMinDate = (minDateStr) => {
+    const { Op } = require('sequelize');
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.findAll({
+            where: {
+                postDate: {
+                    [Op.gte]: new Date(minDateStr)
+                }
+            }
+        })
+            .then((items) => resolve(items))
+            .catch(() => reject('No results returned'));
+    });
 };
 
 module.exports.getItemById = (id) => {
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.findAll({ where: { id } })
+            .then((items) => resolve(items[0]))
+            .catch(() => reject('No results returned'));
+    });
 };
 
-function getPublishedItemsByCategory(category) {
+
+module.exports.getPublishedItemsByCategory(category) {
     return new Promise((resolve, reject) => {
-        reject();
-    })
+        Item.findAll({ where: { published: true, category } })
+            .then((items) => resolve(items))
+            .catch(() => reject('No results returned'));
+    });
 };
